e0bebd28cd6a3cb31ed91e090c3cb6e2e8466a26
Josh Ellman <josh_ellman@taylor.edu>
2024-10-16 09:20:52 -0400

    main commit for depot

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,48 +1,75 @@
 require "test_helper"
 
-class ProductsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @product = products(:one)
+class ProductTest < ActiveSupport::TestCase
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
   end
 
-  test "should get index" do
-    get products_url
-    assert_response :success
+  test "product price must be positive" do
+    product = Product.new(
+      title: "My Book Title",
+      description: "yyy",
+      image_url: "zzz.jpg"
+    )
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 1
+    assert product.valid?
   end
 
-  test "should get new" do
-    get new_product_url
-    assert_response :success
+  test "product is not valid without a unique title" do
+    product = Product.new(
+      title: products(:ruby).title,
+      description: "yyy",
+      price: 1,
+      image_url: "fred.gif"
+    )
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
   end
 
-  test "should create product" do
-    assert_difference("Product.count") do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(
+      title: products(:ruby).title,
+      description: "yyy",
+      price: 1,
+      image_url: "fred.gif"
+    )
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
+
+  def new_product(image_url)
+    Product.new(
+      title: "My Book Title",
+      description: "yyy",
+      price: 1,
+      image_url: image_url
+    )
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} must be valid"
     end
 
-    assert_redirected_to product_url(Product.last)
-  end
-
-  test "should show product" do
-    get product_url(@product)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_product_url(@product)
-    assert_response :success
-  end
-
-  test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
-    assert_redirected_to product_url(@product)
-  end
-
-  test "should destroy product" do
-    assert_difference("Product.count", -1) do
-      delete product_url(@product)
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} must be invalid"
     end
-
-    assert_redirected_to products_url
   end
 end
+
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,73 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Test for the presence of attributes
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  # Test for uniqueness of title
+  test "product is not valid without a unique title" do
+    product = Product.new(
+      title: products(:ruby).title, # Assuming products(:ruby) is defined in your fixtures
+      description: "yyy",
+      price: 1,
+      image_url: "fred.gif"
+    )
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+  # Test for price must be a number
+  test "product price must be a number" do
+    product = Product.new(
+      title: "Valid Title",
+      description: "yyy",
+      price: "not_a_number",
+      image_url: "fred.gif"
+    )
+    assert product.invalid?
+    assert_equal ["is not a number"], product.errors[:price]
+  end
+
+  # Test for price must be greater than or equal to 0.01
+  test "product price must be at least 0.01" do
+    product = Product.new(
+      title: "Valid Title",
+      description: "yyy",
+      price: 0,
+      image_url: "fred.gif"
+    )
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+  end
+
+  # Test for image_url format
+  test "product image_url must be valid format" do
+    product = Product.new(
+      title: "Valid Title",
+      description: "yyy",
+      price: 1,
+      image_url: "invalid_format"
+    )
+    assert product.invalid?
+    assert_equal ["is invalid"], product.errors[:image_url]
+  end
+
+  # Test for valid product creation
+  test "product is valid with valid attributes" do
+    product = Product.new(
+      title: "Unique Title",
+      description: "Valid description",
+      price: 1,
+      image_url: "valid_image.gif"
+    )
+    assert product.valid?
+  end
 end
+
