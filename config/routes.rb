477d789e0a9ad1fe95e54cbfc52bcf764332ddd2
e11man <josh_ellman@icloud.com>
2024-11-06 10:21:35 -0500

    no errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Route for store index
-  get '/store', to: 'store#index'
+  # Route for store index with a named route
+  get '/store', to: 'store#index', as: 'store_index'
   resources :line_items
   resources :carts
   resources :products
